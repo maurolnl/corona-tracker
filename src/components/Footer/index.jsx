3a3d123efed8b547d865082657fbc60f2d89ffa3
maurolnl <mauroleoquiroga@gmail.com>
2021-07-20 17:00:47 -0300

    Add wouter to project and Add a html tag change on footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,14 +3,14 @@ import "./index.css";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <p className="footer-text">
         Made with ♡ by &nbsp;{" "}
         <a href="https:www.github.com/maurolnl" className="footer-link" target="_blank" rel="noopener noreferrer">
           MauroLNL
         </a>
       </p>
-    </div>
+    </footer>
   );
 };
 
